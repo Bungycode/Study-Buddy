d4c3d8b62f2b843e5f312266ff1dd5a25e77966d
Andrew Bungcayao <thedevandrew@gmail.com>
2023-09-22 06:17:35 -0400

    Create MongoDB connection and server initialization with GET/POST test routes. Create not found route for end points that don't exist. Create custom scripts.

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+// Create a connection to database in MongoDB Atlas.
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
